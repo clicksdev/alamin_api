241cc2b7880a2064745cdd3bd2eff419050280fe
kareem22T <kotbekareem74@gmail.com>
2024-06-21 23:39:55 +0300

    edits

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -33,6 +33,12 @@ class HomeController extends Controller
         })->toArray();
 
         $teaser_url = (isset($settingsArray["teaser_url"]) && $settingsArray["teaser_url"]["value"]) ? $settingsArray["teaser_url"]["value"] : '';
+        $google_play_url = (isset($settingsArray["google_play_url"]) && $settingsArray["google_play_url"]["value"]) ? $settingsArray["google_play_url"]["value"] : '';
+        $play_store_url = (isset($settingsArray["play_store_url"]) && $settingsArray["play_store_url"]["value"]) ? $settingsArray["play_store_url"]["value"] : '';
+        $facebook_url = (isset($settingsArray["facebook_url"]) && $settingsArray["facebook_url"]["value"]) ? $settingsArray["facebook_url"]["value"] : '';
+        $instagram_url = (isset($settingsArray["instagram_url"]) && $settingsArray["instagram_url"]["value"]) ? $settingsArray["instagram_url"]["value"] : '';
+        $linkedin_url = (isset($settingsArray["linkedin_url"]) && $settingsArray["linkedin_url"]["value"]) ? $settingsArray["linkedin_url"]["value"] : '';
+
         $main_cat = (isset($settingsArray["main_cat"]) && $settingsArray["main_cat"]["value"]) ? Category::with(["events" => function ($q) {
             $q->latest()->with("location");
         }])->find($settingsArray["main_cat"]["value"]) : null;
@@ -60,6 +66,11 @@ class HomeController extends Controller
             [],
             [
                 "teaser_url" => $teaser_url,
+                "google_play_url" => $google_play_url,
+                "play_store_url" => $play_store_url,
+                "facebook_url" => $facebook_url,
+                "instagram_url" => $instagram_url,
+                "linkedin_url" => $linkedin_url,
                 "main_cat" => $main_cat,
                 "amazing_sponsors" => $amazing_sponsors,
                 'today_events' => $todayEvents,
--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -15,7 +15,7 @@
                 <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Teaser Url</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Main Category Section</button>
+                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Contact</button>
             </li>
             <li class="nav-item" role="presentation">
                 <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Home Restaurants</button>
@@ -28,22 +28,22 @@
                 </div>
             </div>
             <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                @php
-                    $categories = App\Models\Category::all();
-                @endphp
-
                 <div class="form-group">
-                    <label for="main_cat">Choose Category</label>
-                    <select id="main_cat" name="main_cat" class="form-control" value="{{ (isset($settingsArray["main_cat"]) && $settingsArray["main_cat"]["value"]) ? $settingsArray["main_cat"]["value"] : '' }}">
-                        <option value="">Choose Category</option>
-                        @foreach($categories as $cat)
-                            <option value="{{$cat->id}}"
-                                {{ (isset($settingsArray["main_cat"]) && $settingsArray["main_cat"]["value"] == $cat->id) ? "selected" : '' }}>
-                                {{$cat->title}}
-                            </option>
-                        @endforeach
-                    </select>
+                    <input type="text" name="google_play_url" id="google_play_url" class="form-control" placeholder="Google Play Url" value="{{(isset($settingsArray["google_play_url"]) && $settingsArray["google_play_url"]["value"]) ? $settingsArray["google_play_url"]["value"] : ''}}">
                 </div>
+                <div class="form-group">
+                    <input type="text" name="play_store_url" id="play_store_url" class="form-control" placeholder="Play Store Url" value="{{(isset($settingsArray["play_store_url"]) && $settingsArray["play_store_url"]["value"]) ? $settingsArray["play_store_url"]["value"] : ''}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="facebook_url" id="facebook_url" class="form-control" placeholder="Facebook Url" value="{{(isset($settingsArray["facebook_url"]) && $settingsArray["facebook_url"]["value"]) ? $settingsArray["facebook_url"]["value"] : ''}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="instagram_url" id="instagram_url" class="form-control" placeholder="Instagram Url" value="{{(isset($settingsArray["instagram_url"]) && $settingsArray["instagram_url"]["value"]) ? $settingsArray["instagram_url"]["value"] : ''}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="linkedin_url" id="linkedin_url" class="form-control" placeholder="Linked In Url" value="{{(isset($settingsArray["linkedin_url"]) && $settingsArray["linkedin_url"]["value"]) ? $settingsArray["linkedin_url"]["value"] : ''}}">
+                </div>
+
             </div>
             <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
                 @php
