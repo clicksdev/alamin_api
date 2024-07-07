3f76fb26e02415bfbaae8bb8209f1a7fed281755
kareem22T <kotbekareem74@gmail.com>
2024-07-07 15:36:38 +0300

    edits

---
+++ database/migrations/2024_07_07_123548_update_column_subtitle_at_events.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->text('sub_title')->nullable()->change();
+            $table->text('sub_title_ar')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->string('sub_title')->nullable();
+            $table->string('sub_title_ar')->nullable();
+        });
+    }
+};
