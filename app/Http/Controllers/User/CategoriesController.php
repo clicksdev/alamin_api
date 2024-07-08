bb4902cf93ed63f02dd1e85e633ee7b42324bc43
kareem22T <kotbekareem74@gmail.com>
2024-07-08 19:54:08 +0300

    edits

--- app/Http/Controllers/User/CategoriesController.php
+++ app/Http/Controllers/User/CategoriesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\HandleResponseTrait;
 use App\Models\Category;
+use Carbon\Carbon;
 
 class CategoriesController extends Controller
 {
@@ -13,8 +14,26 @@ class CategoriesController extends Controller
 
     public function get() {
         $categories = Category::with(["events" => function($q) {
-            $q->with("location");
+            $q->latest()
+            ->select("id", "title", "sub_title", "title_ar", "sub_title_ar", "cover", "thumbnail", "landscape", "portrait", "url", "date_from", "date_to", "location_id")
+            ->with(['relatedEvents' => function($query) {
+                  $query->select("id", "title", "sub_title", "title_ar", "sub_title_ar", "cover", "thumbnail", "landscape", "portrait", "url", "date_from", "date_to", "location_id")
+                        ->where('date_to', '>=', now()); // Ensure active events only
+              }, "location"]);
         }])->get();
+
+        foreach ($categories as $cat) {
+            foreach ($cat->events as $event) {
+                if ($event) {
+                    $event->time_from = Carbon::parse($event->date_from)->format('h:i A');
+                    $event->time_to = Carbon::parse($event->date_to)->format('h:i A');
+                    foreach ($event->relatedEvents as $relatedEvent) {
+                        $relatedEvent->time_from = Carbon::parse($relatedEvent->date_from)->format('h:i A');
+                        $relatedEvent->time_to = Carbon::parse($relatedEvent->date_to)->format('h:i A');
+                    }
+                }
+            }
+        }
 
         return $this->handleResponse(
             true,
